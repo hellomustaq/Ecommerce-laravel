d95f3b5fe2551eca2b2d526511526a01780b3dae
hellomustaq <hellomstq@gmail.com>
2018-01-31 20:34:51 +0600

    return admin column

--- app/User.php
+++ app/User.php
@@ -26,4 +26,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function isAdmin(){
+      return $this->admin;
+    }
 }
--- database/migrations/2018_01_31_140325_add_admin_column_to_users.php
+++ database/migrations/2018_01_31_140325_add_admin_column_to_users.php
@@ -14,7 +14,7 @@ class AddAdminColumnToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->tinyInteger('admin')->nullable();
+            $table->tinyInteger('admin')->nullable()->after('password');
         });
     }
 
