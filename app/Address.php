261bc6328f27e7ef107a3ca070fb5f7bd1467f3c
Mustaque Ahemmed <hellomstq@gmail.com>
2018-03-05 23:08:49 +0600

    note for address field

--- app/Address.php
+++ app/Address.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    //phone fill optional delete later
     protected $fillable=['address','country','city','state','zip','phone'];
 }
